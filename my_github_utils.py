3e4506ea0fee81a03e3e81645f62c20c35d18b0e
shivakmuddam25 <115982123+shivakmuddam25@users.noreply.github.com>
2022-10-18 23:34:23 +0530

    Add files via upload

---
+++ my_github_utils.py
@@ -0,0 +1,12 @@
+def ROC_Curve(fpr, tpr, roc_auc):
+    import matplotlib.pyplot as plt
+    plt.title('Receiver Operating Characteristic')
+    plt.plot(fpr, tpr, 'b', label = 'AUC = %0.2f' % roc_auc)
+    # plt.plot(pred_prob1[1])
+    plt.legend(loc = 'lower right')
+    plt.plot([0, 1], [0, 1],'r--')
+    plt.xlim([0, 1])
+    plt.ylim([0, 1])
+    plt.ylabel('True Positive Rate')
+    plt.xlabel('False Positive Rate')
+    plt.show()
