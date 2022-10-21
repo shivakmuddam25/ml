48c372b6e934c9ea0582f2f782643d5510cd31e6
shivakmuddam25 <115982123+shivakmuddam25@users.noreply.github.com>
2022-10-21 11:43:55 +0530

    Update my_github_utils.py
    Added a function to find null values and its percentage

--- my_github_utils.py
+++ my_github_utils.py
@@ -24,3 +24,35 @@ def import_basic_libraries():
     from sklearn.linear_model import LinearRegression
     from sklearn.model_selection import train_test_split
     import sklearn
+
+def find_null_values(df_eq_2):
+    ls_nas = [df_eq_2.isna().sum() > 0]
+    ls_nas
+
+    if df_eq_2.isna().sum().sum() == 0:
+        print("No Null values found in the DateFrame")
+    elif df_eq_2.isna().sum().sum() > 0:
+        null_cols = []
+        null_cols_total = []
+        null_cols_bool = []
+        df2_null_per = []
+
+        # Find null cols
+        for col_num in range(len(df_eq_2.columns)):
+            if ls_nas[0][col_num] == True:
+        #         null_cols_bool.append(ls_nas[0][col_num])
+                null_cols.append(df_eq_2.columns[col_num])
+        #         df2_null_per.append(np.round(df_eq_2.columns[col_num].isna().sum()/df_eq_2.shape[0]*100,2))
+
+        #         print(df_eq_2.columns[col_num], " : " + str(ls_nas[0][col_num]))
+
+
+        for null_col in null_cols:
+            df2_null_per.append(np.round(df_eq_2[null_col].isna().sum()/df_eq_2.shape[0]*100 , 2))
+            null_cols_total.append(df_eq_2[null_col].isna().sum())
+        #     print(df_eq_2[null_col].isna().sum())
+
+        # d = pd.DataFrame(list(zip(null_cols, null_cols_bool)), columns= ["Df_2_Null_Cols", "null_cols_bool"])
+        null_cols_per_df2 = pd.DataFrame(list(zip(null_cols, null_cols_total, df2_null_per)),
+                         columns= ["DF Null Cols", "Total Null Values", "Percentage %"]).sort_values(by = ['Percentage %'], ascending = False)
+        return null_cols_per_df2
